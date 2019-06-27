a157058bd8334280f1411a7a5c97be3030926669
gsuamar <guillermosuarezmartinez1@gmail.com>
2019-06-26 18:05:57 -0700

    progress in student class until printEnrollment method

--- Project/src/generalTranscript/GeneralTranscript.java
+++ Project/src/generalTranscript/GeneralTranscript.java
@@ -1,5 +1,18 @@
 package generalTranscript;
 
+import semesterTranscript.SemesterTranscript;
+
 public class GeneralTranscript {
+	public GeneralTranscript() {
+		super();
+		
+	}
+
+	public static SemesterTranscript semesterTranscriptCreator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
 
 }
