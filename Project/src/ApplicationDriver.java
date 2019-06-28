c39b56756a23ed87c0c77ea0abad4fa848ccbe92
gsuamar <guillermosuarezmartinez1@gmail.com>
2019-06-27 22:50:24 -0700

    start loginViewCode

--- Project/src/ApplicationDriver.java
+++ Project/src/ApplicationDriver.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
 
 public class ApplicationDriver {
 	public static void main(String[] args) {
 		System.out.println("Shin make che \n"
 				+ " esy code again");
+	}
+	
+	public void loginView() {
+		boolean passLoginView=true;
+		System.out.println("***************************************************************");
+		System.out.println("Please enter your account to login:");
+		System.out.println("***************************************************************");
+		Scanner s=new Scanner(System.in);
+		System.out.println("Please enter your account to login:");
+		System.out.println("Username:");
+		String username=s.nextLine();
+		System.out.println("Password:");
+		String password=s.nextLine();
+		
 	}
 }
