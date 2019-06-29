c15f59747dea737e32fee3e0c04e965dbcab090f
gsuamar <guillermosuarezmartinez1@gmail.com>
2019-06-28 21:53:54 -0700

    make generalTranscript constructor

--- Project/src/generalTranscript/GeneralTranscript.java
+++ Project/src/generalTranscript/GeneralTranscript.java
@@ -8,35 +8,43 @@ import semesterTranscript.SemesterTranscript;
 import student.Student;
 
 public class GeneralTranscript {
+	
+	HashMap<Course, Integer> courseIntakeSemester;
+	ArrayList<Course> courseList;
+	HashMap<Course, Float> courseGradeMap;
+	static HashMap<Student,Float> allStudentsGpaMap=new HashMap<Student,Float>();
+	
 	public GeneralTranscript() {
 		super();
+		this.courseIntakeSemester=new HashMap<Course, Integer>();
+		this.courseList=new ArrayList<Course>();
+		this.courseGradeMap=new HashMap<Course, Float>();
 		
 	}
+	
 
-	public static SemesterTranscript semesterTranscriptCreator() {
-		// TODO Auto-generated method stub
-		return null;
+	public static void semesterTranscriptCreator() {
+		
+		SemesterTranscript.SemesterTranscript();
 	}
 
 	public ArrayList<Course> getCourseList() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.courseList;
 	}
 
 	public HashMap<Course, Integer> getCourseIntakeSemester() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.courseIntakeSemester;
 	}
 
 	public HashMap<Course, Float> getCourseGradeMap() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.courseGradeMap;
 	}
 
 	public String getMyGPARanking(Student student) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
 
 	
 
