ee381244875d49f03c89c553468287890e693419
gsuamar <guillermosuarezmartinez1@gmail.com>
2019-06-29 23:44:08 -0700

    add the constructor of Account class

--- Project/src/account/Account.java
+++ Project/src/account/Account.java
@@ -1,16 +1,30 @@
 package account;
+import student.*;
 
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Account {
 	private String userName;
 	private String passWord;
-	HashMap<String, String> studentAccounts = new HashMap<String, String>();
+	public static HashMap<String, Student> accountsAndPasswords = new HashMap<String, Student>();
+	//static HashMap<Student,HashMap<String,String>> studentAccountPasswordMap;
 	
-	public Account(String userName, String passWord) {
+	public Account(Student student) {
 		super();
+		Scanner s=new Scanner(System.in);
+		System.out.println("Please username");
+		String userName=s.nextLine();
 		this.userName = userName;
+		System.out.println("Please enter your password");
+		String passWord=s.nextLine();
 		this.passWord = passWord;
+		accountsAndPasswords.put(userName, student);
+//		HashMap<String,String> accountPasswordPair=new HashMap<String,String>();
+//		accountPasswordPair.put(userName, passWord);
+		//studentAccountPasswordMap.put(student, accountPasswordPair);
+		
+		
 	}
 
 	String getUserName() {
